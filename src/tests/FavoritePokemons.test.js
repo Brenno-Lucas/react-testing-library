60197709128b5067d4e6027b28ae65af53b03a59
Brenno-Lucas <brennolucas8@gmail.com>
2022-08-16 18:32:08 -0300

    Requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import FavoritesPokemons from '../pages/FavoritePokemons';
+
+describe('Testes na pagina About', () => {
+  test('Testa se mensagem é exibida, caso a pessoa não tenha pokémons favoritos', () => {
+    render(<FavoritesPokemons />);
+    const notFoundMensage = screen.getByText(/No favorite pokemon found/i);
+    expect(notFoundMensage).toBeInTheDocument();
+  });
+  test('Testa se todos os cards de pokémons favoritados são exibidos.', () => {
+    render(<FavoritesPokemons />);
+    const cards = screen.getByRole('heading', { name: /Favorite pokémons/i });
+    expect(cards).toBeInTheDocument();
+  });
+});
