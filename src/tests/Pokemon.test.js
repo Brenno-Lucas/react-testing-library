fc48a75efb53628d4505b67aa0caf40ffa4b4e42
Brenno-Lucas <brennolucas8@gmail.com>
2022-08-17 11:56:02 -0300

    Correção requisito 06

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import pokemons from '../data';
 import renderWithRouter from '../RWR';
 import { Pokemon } from '../components';
